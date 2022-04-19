351479cd074df9c65368263d1fd5de10019ebcdd
Anurag Singh <contact@anurag.sh>
2022-04-19 21:13:17 +0800

    pub

--- libmaccas/src/api_types.rs
+++ libmaccas/src/api_types.rs
@@ -4,16 +4,16 @@ use serde_derive::Serialize;
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Deal {
-    offer_id: i64,
-    local_valid_from: String,
-    local_valid_to: String,
-    valid_from_utc: String,
-    valid_to_utc: String,
-    name: String,
-    creation_date_utc: String,
+    pub offer_id: i64,
+    pub local_valid_from: String,
+    pub local_valid_to: String,
+    pub valid_from_utc: String,
+    pub valid_to_utc: String,
+    pub name: String,
+    pub creation_date_utc: String,
 }
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct DealResponse {
-    deals: Vec<Deal>,
+    pub deals: Vec<Deal>,
 }
