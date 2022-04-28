edb4b64d548d3f756f0772ec70ad18899b1c454e
Anurag Singh <contact@anurag.sh>
2022-04-28 19:15:12 +0800

    fix ttl

--- maccas-api/core/src/lock.rs
+++ maccas-api/core/src/lock.rs
@@ -43,7 +43,17 @@ pub async fn get_all_locked_deals(
     table_name: &str,
 ) -> Result<Vec<String>, Error> {
     let mut locked_deal_list = Vec::<String>::new();
-    let resp = client.scan().table_name(table_name).send().await?;
+    let utc: DateTime<Utc> = Utc::now();
+    let resp = client
+        .scan()
+        .table_name(table_name)
+        .filter_expression("#ttl_key >= :time")
+        .expression_attribute_names("#ttl_key", "ttl")
+        .expression_attribute_values(":time", AttributeValue::N(utc.timestamp().to_string()))
+        .send()
+        .await?;
+
+    dbg!(&resp);
 
     match resp.items {
         Some(ref items) => {
