9d5e2e6c7c29cc9d891ff48de53a4d5c6913c8a1
Anurag Singh <contact@anurag.sh>
2022-05-26 18:25:26 +0800

    fix for mobile

--- maccas-web/src/hooks/useNotification.tsx
+++ maccas-web/src/hooks/useNotification.tsx
@@ -1,7 +1,5 @@
-import { SnackbarKey, useSnackbar, VariantType } from "notistack";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/material/SvgIcon/SvgIcon";
-import React, { Fragment, useEffect, useState } from "react";
+import { useSnackbar, VariantType } from "notistack";
+import { useEffect, useState } from "react";
 
 interface NotificationConf {
   msg?: string;
@@ -10,24 +8,13 @@ interface NotificationConf {
 
 const useNotification = () => {
   const [conf, setConf] = useState<NotificationConf>({});
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const action = (key: SnackbarKey | undefined) => (
-    <Fragment>
-      <IconButton
-        onClick={() => {
-          closeSnackbar(key);
-        }}
-      >
-        <CloseIcon />
-      </IconButton>
-    </Fragment>
-  );
+  const { enqueueSnackbar } = useSnackbar();
+
   useEffect(() => {
     if (conf?.msg) {
       enqueueSnackbar(conf.msg, {
         variant: conf.variant ?? ("default" as VariantType),
         autoHideDuration: 3000,
-        action,
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
