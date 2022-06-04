ca6714728e28c089cf7a93069581aee7a86adb57
Anurag Singh <contact@anurag.sh>
2022-06-04 22:11:00 +0800

    update public key

--- maccas-web/src/config/api.ts
+++ maccas-web/src/config/api.ts
@@ -1,2 +1,2 @@
-export const PUBLIC_API_KEY = "aa1f0726bdd940f88ed637222d4b4ca3";
+export const PUBLIC_API_KEY = "8853932c44a34f6887dab87c29a38f7c";
 export const IMAGE_BUCKET_BASE = "https://au-prod-us-cds-oceofferimages.s3.amazonaws.com/oce3-au-prod/offers";
