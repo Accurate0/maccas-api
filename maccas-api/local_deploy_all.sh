85e4f49d60f05f0ea321deb89e6da42dbfe6df8b
Anurag Singh <contact@anurag.sh>
2022-06-11 15:17:32 +0800

    one lambda

--- maccas-api/local_deploy_all.sh
+++ maccas-api/local_deploy_all.sh
@@ -3,10 +3,6 @@ pushd api || exit
 cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-v2 --zip-file fileb://target/lambda/api/bootstrap.zip
 popd || exit
 
-pushd deals || exit
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-deals-v2 --zip-file fileb://target/lambda/deals/bootstrap.zip
-popd || exit
-
 pushd refresh || exit
 cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip
 cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip --region ap-southeast-1
