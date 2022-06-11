31aea7613310cc2a1bd90765213ddaace29ecc8d
Anurag Singh <contact@anurag.sh>
2022-06-11 19:36:57 +0800

    significant perf boost

--- maccas-api/api/src/main.rs
+++ maccas-api/api/src/main.rs
@@ -1,4 +1,4 @@
-use lambda_http::{service_fn, Error, IntoResponse, Request};
+use lambda_http::{service_fn, Error, Request};
 use libcore::api::DealsAddRemove;
 use libcore::api::{Code, Deals, DealsLock, LastRefresh, Locations, LocationsSearch, UserConfig};
 use libcore::config::ApiConfig;
@@ -10,11 +10,6 @@ use libcore::logging;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     logging::setup_logging();
-    lambda_http::run(service_fn(run)).await?;
-    Ok(())
-}
-
-async fn run(request: Request) -> Result<impl IntoResponse, Error> {
     let shared_config = aws_config::from_env()
         .region(constants::DEFAULT_AWS_REGION)
         .load()
@@ -22,7 +17,6 @@ async fn run(request: Request) -> Result<impl IntoResponse, Error> {
 
     let config = ApiConfig::load_from_s3(&shared_config).await?;
     let dynamodb_client = aws_sdk_dynamodb::Client::new(&shared_config);
-    request.log();
 
     let mut dispatcher = Dispatcher::new(&config, &dynamodb_client);
 
@@ -35,8 +29,17 @@ async fn run(request: Request) -> Result<impl IntoResponse, Error> {
     dispatcher.add_route("/deals/last-refresh", &LastRefresh);
     dispatcher.add_route("/locations/search", &LocationsSearch);
 
-    let response = dispatcher.dispatch(&request).await?;
-    response.log();
+    let dispatcher = &dispatcher;
 
-    Ok(response)
+    let handler_func_closure = move |request: Request| async move {
+        request.log();
+        let response = dispatcher.dispatch(&request).await?;
+        response.log();
+        Ok(response)
+    };
+
+   // Pass the closure to the runtime here.
+   lambda_http::run(service_fn(handler_func_closure)).await?;
+
+    Ok(())
 }
