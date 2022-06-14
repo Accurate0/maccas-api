552953ecadd0e2603839ea200b9341a502231c2e
Anurag Singh <contact@anurag.sh>
2022-06-14 20:49:38 +0800

    reorg

--- local_deploy_all.sh
+++ local_deploy_all.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-pushd api || exit
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-v2 --zip-file fileb://target/lambda/api/bootstrap.zip
-popd || exit
+cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin api && aws lambda update-function-code --function-name MaccasApi-v2 --zip-file fileb://target/lambda/api/bootstrap.zip
 
-pushd refresh || exit
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip --region ap-southeast-1
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip --region ap-northeast-1
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip --region ap-northeast-2
-popd || exit
+cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin refresh && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip
+cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin refresh && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip --region ap-southeast-1
+cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin refresh && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip --region ap-northeast-1
+cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin refresh && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/refresh/bootstrap.zip --region ap-northeast-2
