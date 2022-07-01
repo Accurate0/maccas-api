98d8828be4cded2628321c733f1a945b296a4e46
Anurag Singh <contact@anurag.sh>
2022-07-01 20:32:37 +0800

    update + fix service

--- src/libapi/middleware.rs
+++ src/libapi/middleware.rs
@@ -6,6 +6,6 @@ pub fn get_middleware_http_client(client: reqwest::Client) -> reqwest_middleware
     let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
     reqwest_middleware::ClientBuilder::new(client)
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
-        .with(TracingMiddleware)
+        .with(TracingMiddleware::default())
         .build()
 }
