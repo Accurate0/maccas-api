74121eb22c5890d1407c8545d42b491ea05b6ef3
Anurag Singh <contact@anurag.sh>
2022-07-17 13:33:35 +0800

    add www auth

--- src/lib/routes/auth_fallback.rs
+++ src/lib/routes/auth_fallback.rs
@@ -1,6 +1,6 @@
 use super::Context;
 use async_trait::async_trait;
-use http::Response;
+use http::{header::WWW_AUTHENTICATE, Response};
 use lambda_http::{Body, Request};
 use simple_dispatcher::{Executor, ExecutorResult};
 
@@ -9,6 +9,9 @@ pub struct AuthFallback;
 #[async_trait]
 impl Executor<Context<'_>, Request, Response<Body>> for AuthFallback {
     async fn execute(&self, _ctx: &Context, _request: &Request) -> ExecutorResult<Response<Body>> {
-        Ok(Response::builder().status(401).body(Body::Empty)?)
+        Ok(Response::builder()
+            .status(401)
+            .header(WWW_AUTHENTICATE, "bearer")
+            .body(Body::Empty)?)
     }
 }
