c504bc23d31cafd0aec339bb2bca01c52e913890
Anurag Singh <contact@anurag.sh>
2022-07-23 22:10:00 +0800

    spacing in log

--- src/lib/logging.rs
+++ src/lib/logging.rs
@@ -17,7 +17,9 @@ use twilight_util::builder::embed::{EmbedBuilder, EmbedFieldBuilder, ImageSource
 
 pub fn setup_logging() {
     fern::Dispatch::new()
-        .format(|out, message, record| out.finish(format_args!("[{}][{}]{}", record.level(), record.target(), message)))
+        .format(|out, message, record| {
+            out.finish(format_args!("[{}][{}] {}", record.level(), record.target(), message))
+        })
         .level(log::LevelFilter::Info)
         .level_for("aws_smithy_http_tower::parse_response", log::LevelFilter::Warn)
         .level_for("aws_config::default_provider::credentials", log::LevelFilter::Warn)
