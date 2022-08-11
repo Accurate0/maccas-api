731124a39390e4950f282c963b5a85a66fd33d4c
Anurag Singh <contact@anurag.sh>
2022-08-11 17:38:52 +0800

    refresh images too

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -53,12 +53,21 @@ async fn run(_: LambdaEvent<Value>) -> Result<Value, anyhow::Error> {
         .refresh_offer_cache(&client_map, &config.ignored_offer_ids)
         .await?;
 
-    if !failed_accounts.is_empty() || !login_failed_accounts.is_empty() {
+    let s3_client = aws_sdk_s3::Client::new(&shared_config);
+    let images_failed_accounts = database
+        .refresh_images(&client_map, &s3_client, &config)
+        .await?;
+
+    if !failed_accounts.is_empty()
+        || !login_failed_accounts.is_empty()
+        || !images_failed_accounts.is_empty()
+    {
         log::error!("refresh failed: {:#?}", failed_accounts);
         log::error!("login failed: {:#?}", login_failed_accounts);
+        log::error!("image failed: {:#?}", images_failed_accounts);
         bail!(
             "{} accounts failed to update",
-            failed_accounts.len() + login_failed_accounts.len()
+            failed_accounts.len() + login_failed_accounts.len() + images_failed_accounts.len()
         )
     }
 
