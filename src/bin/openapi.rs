0e07d186fc8ec3a3041e39dc5c9249ede95910ac
Anurag Singh <contact@anurag.sh>
2022-08-13 23:37:28 +0800

    simplify

--- src/bin/openapi.rs
+++ src/bin/openapi.rs
@@ -1,16 +1,6 @@
 use libapi::routes::docs::openapi::get_openapi;
-use std::env;
-use std::{fs::File, io::Write};
 
-fn main() -> Result<(), anyhow::Error> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    if !args.is_empty() {
-        let output = &args[0];
-        let openapi = get_openapi();
-        let mut file = File::create(output)?;
-        file.write_all(openapi.unwrap().to_pretty_json().unwrap().as_bytes())?;
-    } else {
-        println!("help: openapi [OUTPUT]")
-    }
-    Ok(())
+fn main() {
+    let openapi = get_openapi();
+    println!("{}", openapi.unwrap().to_pretty_json().unwrap());
 }
