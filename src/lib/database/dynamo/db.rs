a1fd24eb15f36312760ac8d142dd296a694fec16
Anurag Singh <contact@anurag.sh>
2022-08-13 13:29:56 +0800

    mv database

--- src/lib/database/dynamo/db.rs
+++ src/lib/database/dynamo/db.rs
@@ -1,11 +1,13 @@
+use super::DynamoDatabase;
 use crate::client;
-use crate::config::{ApiConfig, Tables, UserAccount};
+use crate::config::{ApiConfig, UserAccount};
 use crate::constants::db::{
     ACCESS_TOKEN, ACCOUNT_HASH, ACCOUNT_INFO, ACCOUNT_NAME, CURRENT_LIST, DEAL_UUID, DEVICE_ID,
     LAST_REFRESH, OFFER, OFFER_ID, OFFER_LIST, POINT_INFO, REFRESH_TOKEN, REGION, TTL, USER_CONFIG,
     USER_ID, USER_NAME,
 };
 use crate::constants::mc_donalds::{self, MCDONALDS_IMAGE_BUCKET};
+use crate::database::r#trait::Database;
 use crate::types::api::{Offer, PointsResponse};
 use crate::types::user::UserOptions;
 use crate::utils::{self, get_short_sha1};
@@ -23,120 +25,6 @@ use rand::SeedableRng;
 use std::collections::HashMap;
 use std::time::SystemTime;
 use tokio_stream::StreamExt;
-
-#[async_trait]
-pub trait Database {
-    async fn get_all_offers_as_map(&self) -> Result<HashMap<String, Vec<Offer>>, anyhow::Error>;
-    async fn get_all_offers_as_vec(&self) -> Result<Vec<Offer>, anyhow::Error>;
-    async fn get_offers_for(&self, account_name: &str)
-        -> Result<Option<Vec<Offer>>, anyhow::Error>;
-    async fn set_offers_for(
-        &self,
-        account_name: &str,
-        offer_list: &[Offer],
-    ) -> Result<(), anyhow::Error>;
-    async fn refresh_offer_cache(
-        &self,
-        client_map: &HashMap<UserAccount, ApiClient<'_>>,
-        ignored_offer_ids: &[i64],
-    ) -> Result<Vec<String>, anyhow::Error>;
-    async fn refresh_point_cache_for(
-        &self,
-        account: &UserAccount,
-        api_client: &ApiClient<'_>,
-    ) -> Result<(), anyhow::Error>;
-    async fn get_point_map(&self) -> Result<HashMap<String, PointsResponse>, anyhow::Error>;
-    async fn get_points_by_account_hash(
-        &self,
-        account_hash: &str,
-    ) -> Result<(UserAccount, PointsResponse), anyhow::Error>;
-    async fn refresh_offer_cache_for(
-        &self,
-        account: &UserAccount,
-        api_client: &ApiClient<'_>,
-        ignored_offer_ids: &[i64],
-    ) -> Result<Vec<Offer>, anyhow::Error>;
-    async fn get_refresh_time_for_offer_cache(&self) -> Result<String, anyhow::Error>;
-    async fn get_offer_by_id(&self, offer_id: &str) -> Result<(UserAccount, Offer), anyhow::Error>;
-    async fn get_config_by_user_id(&self, user_id: &str) -> Result<UserOptions, anyhow::Error>;
-    async fn set_config_by_user_id(
-        &self,
-        user_id: &str,
-        user_config: &UserOptions,
-        user_name: &str,
-    ) -> Result<(), anyhow::Error>;
-    async fn get_specific_client<'a>(
-        &self,
-        http_client: &'a reqwest_middleware::ClientWithMiddleware,
-        client_id: &'a str,
-        client_secret: &'a str,
-        sensor_data: &'a str,
-        account: &'a UserAccount,
-        force_login: bool,
-    ) -> Result<ApiClient<'a>, anyhow::Error>;
-    async fn get_client_map<'a>(
-        &self,
-        http_client: &'a reqwest_middleware::ClientWithMiddleware,
-        client_id: &'a str,
-        client_secret: &'a str,
-        sensor_data: &'a str,
-        account_list: &'a [UserAccount],
-        force_login: bool,
-    ) -> Result<(HashMap<UserAccount, ApiClient<'a>>, Vec<String>), anyhow::Error>;
-    async fn lock_deal(&self, deal_id: &str, duration: Duration) -> Result<(), anyhow::Error>;
-    async fn unlock_deal(&self, deal_id: &str) -> Result<(), anyhow::Error>;
-    async fn get_all_locked_deals(&self) -> Result<Vec<String>, anyhow::Error>;
-    async fn delete_all_locked_deals(&self) -> Result<(), anyhow::Error>;
-    async fn get_device_id_for(&self, account_name: &str) -> Result<Option<String>, anyhow::Error>;
-    async fn set_device_id_for(
-        &self,
-        account_name: &str,
-        device_id: &str,
-    ) -> Result<(), anyhow::Error>;
-    async fn increment_refresh_tracking(
-        &self,
-        region: &str,
-        max_count: i8,
-    ) -> Result<i8, anyhow::Error>;
-    async fn refresh_images_for(
-        &self,
-        s3_client: &aws_sdk_s3::Client,
-        config: &ApiConfig,
-        account: &UserAccount,
-    ) -> Result<(), anyhow::Error>;
-    async fn refresh_images(
-        &self,
-        client_map: &HashMap<UserAccount, ApiClient<'_>>,
-        s3_client: &aws_sdk_s3::Client,
-        config: &ApiConfig,
-    ) -> Result<Vec<String>, anyhow::Error>;
-}
-
-pub struct DynamoDatabase {
-    client: aws_sdk_dynamodb::Client,
-    table_name: String,
-    user_config_table_name: String,
-    cache_table_name: String,
-    cache_table_name_v2: String,
-    offer_id_table_name: String,
-    point_table_name: String,
-    refresh_tracking: String,
-}
-
-impl DynamoDatabase {
-    pub fn new(client: aws_sdk_dynamodb::Client, tables: &Tables) -> Self {
-        Self {
-            client,
-            table_name: tables.token_cache.to_owned(),
-            user_config_table_name: tables.user_config.to_owned(),
-            cache_table_name: tables.offer_cache.to_owned(),
-            cache_table_name_v2: tables.offer_cache_v2.to_owned(),
-            offer_id_table_name: tables.offer_id.to_owned(),
-            point_table_name: tables.points.to_owned(),
-            refresh_tracking: tables.refresh_tracking.to_owned(),
-        }
-    }
-}
 
 #[async_trait]
 impl Database for DynamoDatabase {
