b1add2ec806f9c11585c4bc1e1029783bf819628
Anurag Singh <contact@anurag.sh>
2022-08-13 23:26:04 +0800

    rm ts-rs, can use openapi to generate

--- src/lib/types/user.rs
+++ src/lib/types/user.rs
@@ -2,8 +2,6 @@ use serde_derive::Deserialize;
 use serde_derive::Serialize;
 use utoipa::Component;
 
-#[derive(ts_rs::TS)]
-#[ts(export)]
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Component)]
 #[serde(rename_all = "camelCase")]
 pub struct UserOptions {
