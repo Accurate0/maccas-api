6777399854f3d3df34082b00f829be8a1f0facb2
Anurag Singh <contact@anurag.sh>
2022-08-13 23:24:12 +0800

    remove script

--- local_deploy_all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin api && aws lambda update-function-code --function-name MaccasApi-v2 --zip-file fileb://target/lambda/api/bootstrap.zip
-
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin service && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/service/bootstrap.zip
-cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin service && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/service/bootstrap.zip --region ap-southeast-1
-# cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin service && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/service/bootstrap.zip --region ap-northeast-1
-# cargo lambda build --release --target x86_64-unknown-linux-musl --output-format zip --bin service && aws lambda update-function-code --function-name MaccasApi-refresh-v2 --zip-file fileb://target/lambda/service/bootstrap.zip --region ap-northeast-2
