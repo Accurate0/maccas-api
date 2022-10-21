abc2f306cbd0eece4b3ee7716db1a4af45b8d3fa
Anurag Singh <contact@anurag.sh>
2022-10-21 17:49:33 +0800

    add logging

--- src/config.rs
+++ src/config.rs
@@ -9,6 +9,12 @@ impl GeneralConfig {
     async fn load_base_config_from_s3(
         client: &aws_sdk_s3::Client,
     ) -> Result<AggregatedBytes, anyhow::Error> {
+        log::info!(
+            "loading configuration from {}/{}",
+            constants::CONFIG_BUCKET_NAME,
+            constants::config::BASE_FILE
+        );
+
         let resp = client
             .get_object()
             .bucket(constants::CONFIG_BUCKET_NAME)
@@ -42,15 +48,20 @@ impl UserList {
         option: i8,
     ) -> Result<Self, anyhow::Error> {
         let s3_client = aws_sdk_s3::Client::new(shared_config);
+        let user_list_file = constants::config::REGION_ACCOUNTS_FILE
+            .replace("{region}", region)
+            .replace("{option}", &option.to_string());
+
+        log::info!(
+            "loading user list from {}/{}",
+            constants::CONFIG_BUCKET_NAME,
+            user_list_file
+        );
 
         let resp = s3_client
             .get_object()
             .bucket(constants::CONFIG_BUCKET_NAME)
-            .key(
-                constants::config::REGION_ACCOUNTS_FILE
-                    .replace("{region}", region)
-                    .replace("{option}", &option.to_string()),
-            )
+            .key(user_list_file)
             .send()
             .await?;
         let accounts_bytes = resp.body.collect().await?;
