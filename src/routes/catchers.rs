17f5408d29fc5adf5145067d2658df949786a769
Anurag Singh <contact@anurag.sh>
2022-11-13 02:25:27 +0800

    default catcher + logging

--- src/routes/catchers.rs
+++ src/routes/catchers.rs
@@ -1,16 +1,29 @@
-use rocket::Request;
+use rocket::{http::Status, Request};
 
 #[catch(404)]
-pub fn not_found(_req: &Request) -> &'static str {
+pub fn not_found(req: &Request) -> &'static str {
+    log::warn!("response code: {} for request {:#?}", 404, req);
     ""
 }
 
 #[catch(500)]
-pub fn internal_server_error(_req: &Request) -> &'static str {
+pub fn internal_server_error(req: &Request) -> &'static str {
+    log::warn!("response code: {} for request {:#?}", 500, req);
     ""
 }
 
 #[catch(401)]
-pub fn not_authenticated(_req: &Request) -> &'static str {
+pub fn not_authenticated(req: &Request) -> &'static str {
+    log::warn!("response code: {} for request {:#?}", 401, req);
+    ""
+}
+
+#[catch(default)]
+pub fn default(status: Status, req: &Request) -> &'static str {
+    log::warn!(
+        "[default] response code: {:#?} for request {:#?}",
+        status,
+        req
+    );
     ""
 }
