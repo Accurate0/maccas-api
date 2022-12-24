e7b1d792736d037b64c99eec92c09adf9702f662
Anurag Singh <contact@anurag.sh>
2022-12-24 17:07:52 +0800

    spending:wq

--- src/routes/user/spending.rs
+++ src/routes/user/spending.rs
@@ -3,8 +3,9 @@ use crate::{
     guards::authorization::RequiredAuthorizationHeader,
     routes,
     types::api::UserSpending,
-    types::{error::ApiError, jwt::JwtClaim},
+    types::{api::GetDealsOffer, audit::AuditEntry, error::ApiError, jwt::JwtClaim},
 };
+use itertools::Itertools;
 use jwt::{Header, Token};
 use rocket::{serde::json::Json, State};
 
@@ -24,11 +25,51 @@ pub async fn get_user_spending(
     let jwt: Token<Header, JwtClaim, _> = jwt::Token::parse_unverified(&value)?;
     let user_id = &jwt.claims().oid;
 
-    let total_cost = ctx
+    let entries = ctx
         .database
         .get_audit_entries_for(user_id)
         .await
-        .unwrap_or_default()
+        .unwrap_or_default();
+
+    let items = entries
+        .iter()
+        .sorted_by_key(|entry| entry.offer.deal_uuid.clone())
+        .group_by(|entry| entry.offer.deal_uuid.clone())
+        .into_iter()
+        .map(|e| e.1.collect::<Vec<&AuditEntry>>())
+        .collect_vec();
+
+    let mut final_list = Vec::new();
+    for item in &items {
+        let mut list = Vec::new();
+        let all_add_count = item
+            .iter()
+            .filter(|e| e.action == AuditActionType::Add)
+            .collect_vec()
+            .len();
+
+        let all_remove_count = item
+            .iter()
+            .filter(|e| e.action == AuditActionType::Remove)
+            .collect_vec()
+            .len();
+
+        let final_count = all_add_count - all_remove_count;
+
+        if final_count > 0 {
+            list.append(
+                &mut item
+                    .iter()
+                    .take(final_count)
+                    .map(|e| GetDealsOffer::from(e.offer.clone()))
+                    .collect_vec(),
+            );
+        }
+
+        final_list.append(&mut list);
+    }
+
+    let total_cost = entries
         .iter()
         .map(|entry| {
             let price = entry.offer.price.unwrap_or_default();
@@ -39,5 +80,8 @@ pub async fn get_user_spending(
         })
         .sum();
 
-    Ok(Json(UserSpending { total: total_cost }))
+    Ok(Json(UserSpending {
+        total: total_cost,
+        items: final_list,
+    }))
 }
