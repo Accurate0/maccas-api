a1a0e3ee3f96fa5ed514ed8d2c353862662bd806
Anurag <contact@anurag.sh>
2023-01-03 02:04:00 +0800

    Update status.rs

--- src/routes/health/status.rs
+++ src/routes/health/status.rs
@@ -6,7 +6,7 @@ use rocket::http::Status;
         (status = 204, description = "Server is healthy"),
         (status = 500, description = "Internal Server Error"),
     ),
-    tag = "location",
+    tag = "health",
 )]
 #[get("/health/status")]
 pub async fn status() -> Result<Status, ApiError> {
