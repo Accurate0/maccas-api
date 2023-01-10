4619e924d064865189934f16533ad28d31116e58
Anurag Singh <contact@anurag.sh>
2023-01-10 22:22:05 +0800

    reduce nesting, batch size is 1

--- src/bin/cleanup.rs
+++ src/bin/cleanup.rs
@@ -48,93 +48,88 @@ async fn run(event: LambdaEvent<SqsEvent>) -> Result<(), anyhow::Error> {
         })
         .collect();
 
-    // batch size is currently 1 so this loop is redundant..
-    for message in messages {
-        log::info!("request: {:?}", message);
-        if !locked_deals.contains(&message.deal_uuid) {
-            log::warn!("skipping processing of deal - {}", &message.deal_uuid);
-            continue;
-        }
+    // batch size is currently 1
+    let message = messages.first().unwrap();
+    log::info!("request: {:?}", message);
+    if !locked_deals.contains(&message.deal_uuid) {
+        log::warn!("skipping processing of deal - {}", &message.deal_uuid);
+        return Ok(());
+    }
 
-        for _ in 0..MAXIMUM_CLEANUP_RETRY {
-            let (account, offer) = database.get_offer_by_id(&message.deal_uuid).await?;
-            let proxy = proxy::get_proxy(&config);
-            let http_client = foundation::http::get_default_http_client_with_proxy(proxy);
+    let user_id = message.user_id.clone();
+    for _ in 0..MAXIMUM_CLEANUP_RETRY {
+        let (account, offer) = database.get_offer_by_id(&message.deal_uuid).await?;
+        let proxy = proxy::get_proxy(&config);
+        let http_client = foundation::http::get_default_http_client_with_proxy(proxy);
 
-            match database
-                .get_specific_client(
-                    http_client,
-                    &config.mcdonalds.client_id,
-                    &config.mcdonalds.client_secret,
-                    &config.mcdonalds.sensor_data,
-                    &account,
-                    false,
-                )
-                .await
-            {
-                Ok(api_client) => {
-                    log::info!("offer: {:?}", offer);
+        match database
+            .get_specific_client(
+                http_client,
+                &config.mcdonalds.client_id,
+                &config.mcdonalds.client_secret,
+                &config.mcdonalds.sensor_data,
+                &account,
+                false,
+            )
+            .await
+        {
+            Ok(api_client) => {
+                log::info!("offer: {:?}", offer);
 
-                    let deal_stack = api_client
-                        .get_offers_dealstack(default::OFFSET, &message.store_id)
-                        .await?
-                        .body
-                        .response;
+                let deal_stack = api_client
+                    .get_offers_dealstack(default::OFFSET, &message.store_id)
+                    .await?
+                    .body
+                    .response;
 
-                    if deal_stack.is_none() {
-                        log::info!("no deal stack for {}", account);
-                        continue;
-                    }
-
-                    if deal_stack.as_ref().unwrap().deal_stack.is_none() {
-                        log::info!("no deal stack for {}", account);
-                        continue;
-                    }
-
-                    let deal_stack = deal_stack.unwrap().deal_stack.unwrap();
-                    let matched_item = deal_stack.iter().find(|item| {
-                        item.offer_id == offer.offer_id
-                            && item.offer_proposition_id.parse::<i64>()
-                                == Ok(offer.offer_proposition_id)
-                    });
-
-                    match matched_item {
-                        Some(item) => {
-                            let response = api_client
-                                .remove_from_offers_dealstack(
-                                    &item.offer_id,
-                                    &item.offer_proposition_id,
-                                    default::OFFSET,
-                                    &message.store_id,
-                                )
-                                .await?;
-
-                            database
-                                .add_to_audit(
-                                    AuditActionType::Remove,
-                                    message.user_id,
-                                    None,
-                                    &offer,
-                                )
-                                .await;
-
-                            log::info!("removed from dealstack - {}", response.status);
-
-                            database.unlock_deal(&message.deal_uuid).await?;
-                            log::info!("unlocked deal - {}", &message.deal_uuid);
-                        }
-                        None => {
-                            log::info!("no matched item for {:?} - {}", offer, account);
-                        }
-                    }
-
-                    break;
+                if deal_stack.is_none() {
+                    log::info!("no deal stack for {}", account);
+                    continue;
                 }
-                Err(e) => {
-                    log::info!("cleanup failed for {} because {}", message.deal_uuid, e);
+
+                if deal_stack.as_ref().unwrap().deal_stack.is_none() {
+                    log::info!("no deal stack for {}", account);
+                    continue;
                 }
-            };
-        }
+
+                let deal_stack = deal_stack.unwrap().deal_stack.unwrap();
+                let matched_item = deal_stack.iter().find(|item| {
+                    item.offer_id == offer.offer_id
+                        && item.offer_proposition_id.parse::<i64>()
+                            == Ok(offer.offer_proposition_id)
+                });
+
+                match matched_item {
+                    Some(item) => {
+                        let response = api_client
+                            .remove_from_offers_dealstack(
+                                &item.offer_id,
+                                &item.offer_proposition_id,
+                                default::OFFSET,
+                                &message.store_id,
+                            )
+                            .await?;
+
+                        database
+                            .add_to_audit(AuditActionType::Remove, user_id, None, &offer)
+                            .await;
+
+                        log::info!("removed from dealstack - {}", response.status);
+
+                        database.unlock_deal(&message.deal_uuid).await?;
+                        log::info!("unlocked deal - {}", &message.deal_uuid);
+                    }
+                    None => {
+                        log::info!("no matched item for {:?} - {}", offer, account);
+                    }
+                }
+
+                break;
+            }
+            Err(e) => {
+                log::info!("cleanup failed for {} because {}", message.deal_uuid, e);
+            }
+        };
     }
 
     Ok(())
