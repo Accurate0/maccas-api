96abfad4ee1370c3cf90655b988f13f02afe3118
Anurag Singh <contact@anurag.sh>
2023-01-10 22:18:23 +0800

    clean retry too

--- src/bin/cleanup.rs
+++ src/bin/cleanup.rs
@@ -3,6 +3,7 @@ use aws_sdk_dynamodb::Client;
 use foundation::aws;
 use lambda_runtime::service_fn;
 use lambda_runtime::{Error, LambdaEvent};
+use maccas::constants::config::MAXIMUM_CLEANUP_RETRY;
 use maccas::constants::mc_donalds::default;
 use maccas::database::types::AuditActionType;
 use maccas::database::{Database, DynamoDatabase};
@@ -55,67 +56,84 @@ async fn run(event: LambdaEvent<SqsEvent>) -> Result<(), anyhow::Error> {
             continue;
         }
 
-        let (account, offer) = database.get_offer_by_id(&message.deal_uuid).await?;
-        let proxy = proxy::get_proxy(&config);
-        let http_client = foundation::http::get_default_http_client_with_proxy(proxy);
+        for _ in 0..MAXIMUM_CLEANUP_RETRY {
+            let (account, offer) = database.get_offer_by_id(&message.deal_uuid).await?;
+            let proxy = proxy::get_proxy(&config);
+            let http_client = foundation::http::get_default_http_client_with_proxy(proxy);
 
-        let api_client = database
-            .get_specific_client(
-                http_client,
-                &config.mcdonalds.client_id,
-                &config.mcdonalds.client_secret,
-                &config.mcdonalds.sensor_data,
-                &account,
-                false,
-            )
-            .await?;
-        log::info!("offer: {:?}", offer);
+            match database
+                .get_specific_client(
+                    http_client,
+                    &config.mcdonalds.client_id,
+                    &config.mcdonalds.client_secret,
+                    &config.mcdonalds.sensor_data,
+                    &account,
+                    false,
+                )
+                .await
+            {
+                Ok(api_client) => {
+                    log::info!("offer: {:?}", offer);
 
-        let deal_stack = api_client
-            .get_offers_dealstack(default::OFFSET, &message.store_id)
-            .await?
-            .body
-            .response;
+                    let deal_stack = api_client
+                        .get_offers_dealstack(default::OFFSET, &message.store_id)
+                        .await?
+                        .body
+                        .response;
 
-        if deal_stack.is_none() {
-            log::info!("no deal stack for {}", account);
-            continue;
-        }
+                    if deal_stack.is_none() {
+                        log::info!("no deal stack for {}", account);
+                        continue;
+                    }
 
-        if deal_stack.as_ref().unwrap().deal_stack.is_none() {
-            log::info!("no deal stack for {}", account);
-            continue;
-        }
+                    if deal_stack.as_ref().unwrap().deal_stack.is_none() {
+                        log::info!("no deal stack for {}", account);
+                        continue;
+                    }
 
-        let deal_stack = deal_stack.unwrap().deal_stack.unwrap();
-        let matched_item = deal_stack.iter().find(|item| {
-            item.offer_id == offer.offer_id
-                && item.offer_proposition_id.parse::<i64>() == Ok(offer.offer_proposition_id)
-        });
+                    let deal_stack = deal_stack.unwrap().deal_stack.unwrap();
+                    let matched_item = deal_stack.iter().find(|item| {
+                        item.offer_id == offer.offer_id
+                            && item.offer_proposition_id.parse::<i64>()
+                                == Ok(offer.offer_proposition_id)
+                    });
 
-        match matched_item {
-            Some(item) => {
-                let response = api_client
-                    .remove_from_offers_dealstack(
-                        &item.offer_id,
-                        &item.offer_proposition_id,
-                        default::OFFSET,
-                        &message.store_id,
-                    )
-                    .await?;
+                    match matched_item {
+                        Some(item) => {
+                            let response = api_client
+                                .remove_from_offers_dealstack(
+                                    &item.offer_id,
+                                    &item.offer_proposition_id,
+                                    default::OFFSET,
+                                    &message.store_id,
+                                )
+                                .await?;
 
-                database
-                    .add_to_audit(AuditActionType::Remove, message.user_id, None, &offer)
-                    .await;
+                            database
+                                .add_to_audit(
+                                    AuditActionType::Remove,
+                                    message.user_id,
+                                    None,
+                                    &offer,
+                                )
+                                .await;
 
-                log::info!("removed from dealstack - {}", response.status);
+                            log::info!("removed from dealstack - {}", response.status);
 
-                database.unlock_deal(&message.deal_uuid).await?;
-                log::info!("unlocked deal - {}", &message.deal_uuid);
-            }
-            None => {
-                log::info!("no matched item for {:?} - {}", offer, account);
-            }
+                            database.unlock_deal(&message.deal_uuid).await?;
+                            log::info!("unlocked deal - {}", &message.deal_uuid);
+                        }
+                        None => {
+                            log::info!("no matched item for {:?} - {}", offer, account);
+                        }
+                    }
+
+                    break;
+                }
+                Err(e) => {
+                    log::info!("cleanup failed for {} because {}", message.deal_uuid, e);
+                }
+            };
         }
     }
 
--- src/constants.rs
+++ src/constants.rs
@@ -29,6 +29,7 @@ pub mod config {
     pub const DEFAULT_REFRESH_TTL_HOURS: i64 = 24;
     pub const DEFAULT_LOCK_TTL_HOURS: i64 = 12;
     pub const MAXIMUM_FAILURE_HANDLER_RETRY: i8 = 5;
+    pub const MAXIMUM_CLEANUP_RETRY: i8 = 5;
 }
 
 pub mod db {
