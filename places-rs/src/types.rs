38969bab12f1160b65eae1676c9225ef96161f5f
Anurag Singh <contact@anurag.sh>
2023-07-31 21:58:31 +0800

    they dont use camel case

--- places-rs/src/types.rs
+++ places-rs/src/types.rs
@@ -40,37 +40,31 @@ pub enum ClientError {
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct PlacesResponse {
     pub status: String,
     pub candidates: Vec<Place>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Place {
-    #[serde(rename = "formatted_address")]
     pub formatted_address: String,
     pub geometry: Geometry,
     pub name: String,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Geometry {
     pub location: Location,
     pub viewport: Viewport,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Location {
     pub lat: f64,
     pub lng: f64,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Viewport {
     pub northeast: Location,
     pub southwest: Location,
