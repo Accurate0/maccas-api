9d840c06d4b14f23808c038ca653e785b3bf8844
Anurag Singh <contact@anurag.sh>
2023-09-03 18:05:34 +0800

    add infra

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.6.2"
+    }
+  }
+
+  backend "s3" {
+    key            = "maccas/api-gateway.tfstate"
+    bucket         = "shared-tf-state"
+    region         = "ap-southeast-2"
+    encrypt        = true
+    dynamodb_table = "shared-tf-state-lock"
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      Project = "Maccas API"
+    }
+  }
+}
---
+++ infra/vars.tf
@@ -0,0 +1,24 @@
+variable "API_GATEWAY_ID" {
+  type    = string
+  default = null
+}
+
+variable "API_GATEWAY_ID_DEV" {
+  type    = string
+  default = null
+}
+
+variable "AWS_INTEGRATION_ID" {
+  type    = string
+  default = null
+}
+
+variable "AWS_INTEGRATION_ID_DEV" {
+  type    = string
+  default = null
+}
+
+variable "ADB2C_APPLICATION_ID" {
+  type    = string
+  default = null
+}
