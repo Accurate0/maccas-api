a0fb14230ed1bc52d92f6a24148a2d7e9d877055
Anurag Singh <contact@anurag.sh>
2023-12-17 13:19:28 +0800

    nullable

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -7,6 +7,7 @@ mod m20231214_063221_add_accounts_table;
 mod m20231214_080919_add_jobs_table;
 mod m20231216_133706_offer_details_table;
 mod m20231216_140842_add_fk_offer_details;
+mod m20231217_051327_update_field;
 
 pub struct Migrator;
 
@@ -21,6 +22,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20231214_080919_add_jobs_table::Migration),
             Box::new(m20231216_133706_offer_details_table::Migration),
             Box::new(m20231216_140842_add_fk_offer_details::Migration),
+            Box::new(m20231217_051327_update_field::Migration),
         ]
     }
 }
