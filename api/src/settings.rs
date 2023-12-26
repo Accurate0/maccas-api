88da7d344e006a79d833fc3d52883d77407325b9
Anurag Singh <contact@anurag.sh>
2023-12-26 18:46:07 +0800

    save 2

---
+++ api/src/settings.rs
@@ -0,0 +1,42 @@
+use config::{Config, ConfigError, Environment, File};
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+pub struct Database {
+    pub url: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Proxy {
+    pub username: String,
+    pub password: String,
+    pub url: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct McDonalds {
+    pub client_id: String,
+    pub client_secret: String,
+    pub sensor_data: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub database: Database,
+    pub proxy: Proxy,
+    pub mcdonalds: McDonalds,
+    pub downstream_health_checks: HashMap<String, String>,
+    pub image_host_prefix: String,
+}
+
+impl Settings {
+    pub fn new() -> Result<Self, ConfigError> {
+        let s = Config::builder()
+            .add_source(File::with_name("api.config.toml").required(true))
+            .add_source(Environment::default().separator("__"))
+            .build()?;
+
+        s.try_deserialize()
+    }
+}
