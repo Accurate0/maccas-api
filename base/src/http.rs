33b63e990e8dbf302c80b07a660cfac6be654cb2
Anurag Singh <contact@anurag.sh>
2023-12-26 17:32:34 +0800

    improve

--- base/src/http.rs
+++ base/src/http.rs
@@ -1,11 +1,17 @@
-use reqwest::{Proxy, StatusCode};
-use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest::{Proxy, Request, Response, StatusCode};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware, Extension};
 use reqwest_retry::{
     default_on_request_failure, default_on_request_success, policies::ExponentialBackoff,
     RetryTransientMiddleware, Retryable, RetryableStrategy,
 };
-use reqwest_tracing::TracingMiddleware;
+use reqwest_tracing::{
+    default_on_request_end, reqwest_otel_span, DisableOtelPropagation, ReqwestOtelSpanBackend,
+    TracingMiddleware,
+};
+use std::time::Instant;
+use task_local_extensions::Extensions;
 use thiserror::Error;
+use tracing::Span;
 
 pub struct AkamaiCdnRetryStrategy;
 
@@ -27,6 +33,35 @@ impl RetryableStrategy for AkamaiCdnRetryStrategy {
     }
 }
 
+pub struct TimeTrace;
+impl ReqwestOtelSpanBackend for TimeTrace {
+    fn on_request_start(req: &Request, extension: &mut Extensions) -> Span {
+        extension.insert(Instant::now());
+        let url = req.url().to_string();
+        let method = req.method().to_string();
+        let name = format!("{method} {url}");
+
+        tracing::info!("starting request for {name}");
+        reqwest_otel_span!(
+            name = "http",
+            req,
+            time_elapsed = tracing::field::Empty,
+            url
+        )
+    }
+
+    fn on_request_end(
+        span: &Span,
+        outcome: &reqwest_middleware::Result<Response>,
+        extension: &mut Extensions,
+    ) {
+        let time_elapsed = extension.get::<Instant>().unwrap().elapsed().as_millis() as i64;
+        default_on_request_end(span, outcome);
+        span.record("time_elapsed", format!("{time_elapsed}ms"));
+        tracing::info!("finished request for {:?}", span.field("name"));
+    }
+}
+
 #[derive(Error, Debug)]
 pub enum HttpCreationError {
     #[error("Request builder error has occurred: `{0}`")]
@@ -38,7 +73,8 @@ pub fn get_http_client(proxy: Proxy) -> Result<ClientWithMiddleware, HttpCreatio
 
     Ok(
         ClientBuilder::new(reqwest::ClientBuilder::new().proxy(proxy).build()?)
-            .with(TracingMiddleware::default())
+            .with_init(Extension(DisableOtelPropagation))
+            .with(TracingMiddleware::<TimeTrace>::new())
             .with(RetryTransientMiddleware::new_with_policy_and_strategy(
                 retry_policy,
                 AkamaiCdnRetryStrategy,
