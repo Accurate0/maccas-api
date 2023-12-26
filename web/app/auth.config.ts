2a802c762e8e675dd98a3da7b4b28edad88f1acb
Anurag Singh <contact@anurag.sh>
2023-12-27 00:48:58 +0800

    we in the future now

---
+++ web/app/auth.config.ts
@@ -0,0 +1,37 @@
+import { prisma } from "@/prisma/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authConfig = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+        // check if user exists on current server
+        // create new if it does
+        // otherwise check own
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+};
