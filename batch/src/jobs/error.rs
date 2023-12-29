5211efb6db344bce9a3b6aa4bbea0ab08f040472
Anurag Singh <contact@anurag.sh>
2023-12-30 00:58:37 +0800

    event based

--- batch/src/jobs/error.rs
+++ batch/src/jobs/error.rs
@@ -1,12 +1,17 @@
 use sea_orm::DbErr;
 use thiserror::Error;
+use tokio::sync::mpsc::error::SendError;
+
+use super::job_scheduler;
 
 #[derive(Error, Debug)]
 pub enum JobError {
     #[error("Database error has ocurred: `{0}`")]
-    DatabaseError(#[from] DbErr),
+    Database(#[from] DbErr),
     #[error("Serialization error has occurred: `{0}`")]
-    SerializationError(#[from] serde_json::Error),
-    // #[error("unknown error")]
-    // Unknown,
+    Serialization(#[from] serde_json::Error),
+    #[error("Chrono error has occurred: `{0}`")]
+    Chrono(#[from] chrono::OutOfRangeError),
+    #[error("Send error has occurred: `{0}`")]
+    Send(#[from] SendError<job_scheduler::Message>),
 }
