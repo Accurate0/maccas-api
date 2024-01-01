7ed68a92b0b30489ce1d9dc6dbf040c186db2681
Anurag Singh <contact@anurag.sh>
2024-01-01 18:45:46 +0800

    practically building a framework

--- event/src/event_manager/handlers/cleanup.rs
+++ event/src/event_manager/handlers/cleanup.rs
@@ -1,9 +1,9 @@
 use super::HandlerError;
-use event::CreateEvent;
+// use event::CreateEvent;
 use sea_orm::DatabaseConnection;
 
 pub async fn cleanup(offer_id: String, _db: DatabaseConnection) -> Result<(), HandlerError> {
     tracing::info!("cleanup for {}", offer_id);
-    serde_json::from_str::<CreateEvent>("x")?;
+    // serde_json::from_str::<CreateEvent>("x")?;
     Ok(())
 }
