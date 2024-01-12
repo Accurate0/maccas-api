bfc2ea4bb34ee7f8c36a46e72fc1b49e406502f2
Anurag Singh <contact@anurag.sh>
2024-01-12 22:06:00 +0800

    some changes

--- api/src/types.rs
+++ api/src/types.rs
@@ -10,10 +10,8 @@ pub struct ApiState {
     pub settings: Settings,
 }
 
-// Make our own error that wraps `anyhow::Error`.
 pub struct AppError(anyhow::Error);
 
-// Tell axum how to convert `AppError` into a response.
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         (
@@ -24,8 +22,6 @@ impl IntoResponse for AppError {
     }
 }
 
-// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
-// `Result<_, AppError>`. That way you don't need to do that manually.
 impl<E> From<E> for AppError
 where
     E: Into<anyhow::Error>,
