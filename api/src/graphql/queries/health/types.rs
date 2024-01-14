6650ab4f6a3944d6909392f27af30877f8ab312e
Anurag Singh <contact@anurag.sh>
2024-01-14 14:37:35 +0800

    add health check

--- api/src/graphql/queries/health/types.rs
+++ api/src/graphql/queries/health/types.rs
@@ -1,6 +1,27 @@
-use async_graphql::SimpleObject;
+use async_graphql::Object;
+use event::Health;
+use reqwest::StatusCode;
+use reqwest_middleware::ClientWithMiddleware;
+use sea_orm::DatabaseConnection;
 
-#[derive(SimpleObject)]
-pub struct HealthResponse {
-    pub database_healthy: bool,
+use crate::settings::Settings;
+
+pub struct HealthResponse;
+
+#[Object]
+impl HealthResponse {
+    pub async fn database(&self, ctx: &async_graphql::Context<'_>) -> async_graphql::Result<bool> {
+        let db = ctx.data::<DatabaseConnection>()?;
+        Ok(db.ping().await.is_ok())
+    }
+
+    pub async fn event(&self, ctx: &async_graphql::Context<'_>) -> async_graphql::Result<bool> {
+        let settings = ctx.data::<Settings>()?;
+        let http_client = ctx.data::<ClientWithMiddleware>()?;
+
+        let request_url = format!("{}/{}", settings.event_api_base, Health::path());
+        let event_health_response = http_client.get(request_url).send().await;
+
+        Ok(event_health_response.is_ok_and(|r| r.status() == StatusCode::NO_CONTENT))
+    }
 }
--- event/src/routes/mod.rs
+++ event/src/routes/mod.rs
@@ -1,1 +1,2 @@
 pub mod create_event;
+pub mod health;
