97ab711f507667d89c3d94ff712487ee92f75065
Anurag Singh <contact@anurag.sh>
2024-01-26 15:31:01 +0800

    fixes

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -4,9 +4,6 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
