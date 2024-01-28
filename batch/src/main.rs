8d939ad22e77871ed0eb0803d513d1e962f5db34
Anurag Singh <contact@anurag.sh>
2024-01-28 20:53:50 +0800

    30 mins

--- batch/src/main.rs
+++ batch/src/main.rs
@@ -64,7 +64,7 @@ async fn main() -> Result<(), anyhow::Error> {
                 http_client: http_client.clone(),
                 mcdonalds_config: settings.mcdonalds.clone(),
             },
-            "0 * */4 * * *".parse()?,
+            "0 */30 * * * *".parse()?,
         )
         .await;
 
