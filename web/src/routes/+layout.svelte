c5320a74b6dd2e8160b09d29c07573fbd8fe4c9f
Anurag Singh <contact@anurag.sh>
2024-01-28 21:33:28 +0800

    add view transition

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.pcss';
+
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) {
+			return;
+		}
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <h2 class="m-4 mb-1 p-1 text-3xl font-bold tracking-tight">Maccas</h2>
