f84c36f46e8885e89f6351bba3dbc37b14d84e64
Anurag Singh <contact@anurag.sh>
2024-01-28 18:25:40 +0800

    fixes

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.pcss';
 </script>
 
-<div class="flex h-screen justify-center">
+<h2 class="m-4 mb-1 p-1 text-3xl font-bold tracking-tight">Maccas</h2>
+<div class="flex h-full justify-center">
 	<slot />
 </div>
