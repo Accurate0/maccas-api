b4129c1dab853b7fe621ecc33ab276a3df93c49f
Anurag Singh <contact@anurag.sh>
2024-01-28 18:03:31 +0800

    fix hydration issue

--- dashboard/src/components/time-in-seconds-future.tsx
+++ dashboard/src/components/time-in-seconds-future.tsx
@@ -8,7 +8,7 @@ export const TimeSecondsInFuture = ({
   const dateTime = new Date(Date.now() + secondsInFuture * 1000);
 
   return (
-    <time dateTime={dateTime.toISOString()}>
+    <time dateTime={dateTime.toISOString()} suppressHydrationWarning>
       {dateTime.toLocaleString("en-AU")}
     </time>
   );
