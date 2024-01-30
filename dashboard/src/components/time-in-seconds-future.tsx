f09a1addd665fbdb4a79b38415df2cf67e9627fa
Anurag Singh <contact@anurag.sh>
2024-01-30 20:44:06 +0800

    hardmode prevent ssr

--- dashboard/src/components/time-in-seconds-future.tsx
+++ dashboard/src/components/time-in-seconds-future.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import { Suspense } from "react";
-import { useHydration } from "./use-hydration";
-
 export const TimeSecondsInFuture = ({
   secondsInFuture,
 }: {
   secondsInFuture: number;
 }) => {
   const dateTime = new Date(Date.now() + secondsInFuture * 1000);
-  const hydrated = useHydration();
 
   return (
-    <Suspense key={hydrated ? "local" : "utc"}>
-      <time dateTime={dateTime.toISOString()}>
-        {dateTime.toLocaleString("en-AU")}
-      </time>
-    </Suspense>
+    <time dateTime={dateTime.toISOString()}>
+      {dateTime.toLocaleString("en-AU")}
+    </time>
   );
 };
