45bd3f4d2cbbf605dcd6a0a856549fa5177b3575
Anurag Singh <contact@anurag.sh>
2024-01-30 20:33:22 +0800

    whatever that is

--- dashboard/src/components/time.tsx
+++ dashboard/src/components/time.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { parseISO } from "date-fns";
+import { useHydration } from "./use-hydration";
+import { Suspense } from "react";
 
-export const Time = ({ datetime }: { datetime: string | number }) => {
+export const Time = ({ datetime }: { datetime: string }) => {
   // TODO: FIXME
   const dateTime = parseISO(datetime + "Z");
+  const hydrated = useHydration();
 
   return (
-    <time dateTime={dateTime.toISOString()} suppressHydrationWarning>
-      {dateTime.toLocaleString("en-AU")}
-    </time>
+    <Suspense key={hydrated ? "local" : "utc"}>
+      <time dateTime={dateTime.toISOString()}>
+        {dateTime.toLocaleString("en-AU")}
+      </time>
+    </Suspense>
   );
 };
