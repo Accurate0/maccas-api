a18db024166f49a2f2dab7c087063d3c4afd33e4
Anurag Singh <contact@anurag.sh>
2024-01-30 19:50:03 +0800

    add darkmode toggle

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -10,6 +10,10 @@
 	import type { LayoutData } from './$types';
 	import { derived } from 'svelte/store';
 	import { configStore } from '$lib/config';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Sun, Moon } from 'radix-icons-svelte';
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
 
 	const queryClient = new QueryClient();
 	export let data: LayoutData;
@@ -38,22 +42,40 @@
 </svelte:head>
 
 <Toaster richColors />
+<ModeWatcher />
 
 <h2 class="m-4 mb-1 p-1 text-3xl font-bold tracking-tight">
 	<a href="/">Maccas</a>
 </h2>
 <div class="flex h-full justify-center">
 	<QueryClientProvider client={queryClient}>
-		<Tabs.Root value={$page.route.id?.replace('/', '') ?? undefined} class="w-[100%]">
-			{#if !data.hideAll}
-				<Tabs.List class="m-4 mb-0">
-					<Tabs.Trigger on:click={() => goto('/offers')} value="offers">Offers</Tabs.Trigger>
-					{#if data.showPoints}
-						<Tabs.Trigger on:click={() => goto('/points')} value="points">Points</Tabs.Trigger>
+		<div class="w-full">
+			<div class="space-bet flex justify-between align-baseline">
+				<Tabs.Root value={$page.route.id?.replace('/', '') ?? undefined}>
+					{#if !data.hideAll}
+						<Tabs.List class="m-4 mb-0">
+							<Tabs.Trigger on:click={() => goto('/offers')} value="offers">Offers</Tabs.Trigger>
+							{#if data.showPoints}
+								<Tabs.Trigger on:click={() => goto('/points')} value="points">Points</Tabs.Trigger>
+							{/if}
+							<Tabs.Trigger on:click={() => goto('/location')} value="location"
+								>Location</Tabs.Trigger
+							>
+						</Tabs.List>
 					{/if}
-					<Tabs.Trigger on:click={() => goto('/location')} value="location">Location</Tabs.Trigger>
-				</Tabs.List>
-			{/if}
+				</Tabs.Root>
+				<div class="m-4 mb-0">
+					<Button on:click={toggleMode} variant="outline" size="icon">
+						<Sun
+							class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+						/>
+						<Moon
+							class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+						/>
+						<span class="sr-only">Toggle theme</span>
+					</Button>
+				</div>
+			</div>
 
 			{#if $storeName && !data.hideAll}
 				<div class="m-4 grid grid-flow-row gap-4">
@@ -70,6 +92,6 @@
 			<div class="m-4 grid grid-flow-row gap-4">
 				<slot />
 			</div>
-		</Tabs.Root>
+		</div>
 	</QueryClientProvider>
 </div>
