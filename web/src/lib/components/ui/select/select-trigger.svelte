c8870273b41362753bee32e794537c5b50b7ff78
Anurag Singh <contact@anurag.sh>
2024-02-03 21:22:21 +0800

    same thing

--- web/src/lib/components/ui/select/select-trigger.svelte
+++ web/src/lib/components/ui/select/select-trigger.svelte
@@ -12,7 +12,7 @@
 
 <SelectPrimitive.Trigger
 	class={cn(
-		'border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring grid h-9 w-full grid-flow-col items-center justify-between whitespace-nowrap rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+		'border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
 		className
 	)}
 	{...$$restProps}
