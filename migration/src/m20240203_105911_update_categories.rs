e4f358c0f0f241362845f85ec95097be438a2934
Anurag Singh <contact@anurag.sh>
2024-02-03 19:46:45 +0800

    missing file

---
+++ migration/src/m20240203_105911_update_categories.rs
@@ -0,0 +1,48 @@
+use entity::categories;
+use sea_orm_migration::{
+    prelude::*,
+    sea_orm::{EntityTrait, Set, TransactionTrait},
+};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(DeriveIden)]
+enum Categories {
+    Table,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        let categories = ["Breakfast", "Drinks", "Desserts", "Chicken", "Beef", "Fish"];
+
+        manager
+            .truncate_table(Table::truncate().table(Categories::Table).to_owned())
+            .await?;
+
+        let txn = db.begin().await?;
+        let category_models = categories.map(|c| categories::ActiveModel {
+            name: Set(c.to_owned()),
+            ..Default::default()
+        });
+
+        categories::Entity::insert_many(category_models)
+            .on_conflict(
+                OnConflict::column(categories::Column::Name)
+                    .do_nothing()
+                    .to_owned(),
+            )
+            .exec_without_returning(&txn)
+            .await?;
+
+        txn.commit().await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
