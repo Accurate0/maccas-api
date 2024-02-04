d33265d7c9a747d2844dc0eb09ef9078961d2df2
Anurag Singh <contact@anurag.sh>
2024-02-04 16:24:34 +0800

    remove server header

--- web/docker-entry.js
+++ web/docker-entry.js
@@ -8,6 +8,20 @@ app.get('/health', (req, res) => {
 });
 
 app.use(handler);
+app.use(function (req, res, next) {
+	res.removeHeader('Server');
+	next();
+});
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+function shutdown() {
+	console.log('graceful shutdown express');
+	app.close(function () {
+		console.log('closed express');
+	});
+}
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
