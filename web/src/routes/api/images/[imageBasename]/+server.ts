87de6a033fa7b7f8a28849ee4d61ba19563f9552
Anurag Singh <contact@anurag.sh>
2024-02-24 01:22:55 +0800

    transform images + fix skeleton

--- web/src/routes/api/images/[imageBasename]/+server.ts
+++ web/src/routes/api/images/[imageBasename]/+server.ts
@@ -1,4 +1,5 @@
 import { IMAGE_CDN } from '$lib/constants';
+import sharp from 'sharp';
 
 export async function GET({ fetch, params: { imageBasename } }) {
 	const response = await fetch(`${IMAGE_CDN}/${imageBasename}`, {
@@ -7,10 +8,15 @@ export async function GET({ fetch, params: { imageBasename } }) {
 		referrer: ''
 	});
 
-	const imageResponse = new Response(response.body, {
+	const image = await sharp(await response.arrayBuffer())
+		.resize(180, 180)
+		.jpeg({ mozjpeg: true })
+		.toBuffer();
+
+	const imageResponse = new Response(image, {
 		headers: {
 			'cache-control': 'max-age=604800',
-			'content-type': response.headers.get('content-type') ?? ''
+			'content-type': 'image/jpeg'
 		}
 	});
 
