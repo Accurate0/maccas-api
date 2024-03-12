809b23d2583a829ad5a539f56c9b91a8650125a7
Anurag Singh <contact@anurag.sh>
2024-03-12 21:38:59 +0800

    both

--- api/src/main.rs
+++ api/src/main.rs
@@ -58,6 +58,10 @@ async fn main() -> Result<(), anyhow::Error> {
         },
         tokio::spawn,
     ))
+    .extension(async_graphql::extensions::Tracing)
+    .extension(async_graphql::extensions::OpenTelemetry::new(
+        opentelemetry::global::tracer("graphql"),
+    ))
     .finish();
 
     let cors = CorsLayer::new()
