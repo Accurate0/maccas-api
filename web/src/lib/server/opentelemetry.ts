f93b18b1ab900b46dc94f0045ba6fd4d85f0663f
Anurag Singh <contact@anurag.sh>
2024-03-14 20:36:23 +0800

    e2e tracing

--- web/src/lib/server/opentelemetry.ts
+++ web/src/lib/server/opentelemetry.ts
@@ -7,16 +7,23 @@ import {
 import { NODE_ENV } from '$env/static/private';
 import { env } from '$env/dynamic/private';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 
+import pkg from '@prisma/instrumentation';
+const { PrismaInstrumentation } = pkg;
+
+const traceExporter = new OTLPTraceExporter({
+	url: 'https://otel.highlight.io:4318/v1/traces'
+});
 const otelSdk = new opentelemetry.NodeSDK({
 	resource: new Resource({
 		[SEMRESATTRS_SERVICE_NAME]: 'web',
 		[SEMRESATTRS_DEPLOYMENT_ENVIRONMENT]: NODE_ENV,
 		['highlight.project_id']: env.OTEL_PROJECT_ID
 	}),
-	traceExporter: new OTLPTraceExporter({
-		url: 'https://otel.highlight.io:4318/v1/traces'
-	})
+	spanProcessors: [new BatchSpanProcessor(traceExporter)],
+	traceExporter,
+	instrumentations: [new PrismaInstrumentation()]
 });
 
 otelSdk.start();
