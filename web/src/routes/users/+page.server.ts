1797787f3df46f1f8d22d8cf1d4f68cc4ac8a79a
Anurag Singh <contact@anurag.sh>
2024-03-19 20:48:29 +0800

    fix

--- web/src/routes/users/+page.server.ts
+++ web/src/routes/users/+page.server.ts
@@ -4,14 +4,14 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async (event) => {
 	const user = await prisma.user.findUniqueOrThrow({ where: { id: event.locals.session.userId } });
-	if (user.role !== Role.ADMIN) {
+	if (user.role.some((role) => role === Role.ADMIN)) {
 		redirect(303, '/');
 	}
 
 	return {
 		users: prisma.user
 			.findMany()
-			.then((p) => p.filter((u) => u.role !== Role.ADMIN))
+			.then((p) => p.filter((u) => u.role.some((role) => role === Role.ADMIN)))
 			.then((p) => p.map((u) => ({ username: u.username, active: u.active, id: u.id })))
 	};
 };
