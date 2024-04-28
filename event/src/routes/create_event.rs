dec6a471b0718e3b935318895d11a454f600ed3b
Anurag Singh <contact@anurag.sh>
2024-04-28 18:06:20 +0800

    trace parent fix

--- event/src/routes/create_event.rs
+++ event/src/routes/create_event.rs
@@ -1,11 +1,13 @@
 use crate::{error::EventError, state::AppState};
-use actix_web::web::{self};
+use actix_web::web;
 use event::{CreateEvent, CreateEventResponse};
 
 pub(crate) async fn create_event(
     state: web::Data<AppState>,
     request: web::Json<CreateEvent>,
+    req: actix_web::HttpRequest,
 ) -> Result<CreateEventResponse, EventError> {
+    tracing::info!("{:#?}", req.headers().get("traceparent"));
     let id = state
         .event_manager
         .create_event(request.event.clone(), request.delay)
