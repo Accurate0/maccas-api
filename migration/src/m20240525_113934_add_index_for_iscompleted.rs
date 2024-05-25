fd98a12860c6a60cc4e543b1402c7879289487d9
Anurag Singh <contact@anurag.sh>
2024-05-25 19:59:23 +0800

    add index for future

---
+++ migration/src/m20240525_113934_add_index_for_iscompleted.rs
@@ -0,0 +1,40 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(DeriveIden)]
+enum Events {
+    Table,
+    IsCompleted,
+}
+
+// This index helps the is completed table scan
+// when reloading incomplete events
+const INDEX_NAME: &str = "idx_events_iscompleted";
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_index(
+                Index::create()
+                    .table(Events::Table)
+                    .name(INDEX_NAME)
+                    .col(Events::IsCompleted)
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(INDEX_NAME)
+                    .table(Events::Table)
+                    .to_owned(),
+            )
+            .await
+    }
+}
