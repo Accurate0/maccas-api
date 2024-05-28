42288eb2daf9991dede11180eba779314f08ee42
Anurag Singh <contact@anurag.sh>
2024-05-28 21:46:01 +0800

    name it unstable

--- event/src/result_extension.rs
+++ event/src/result_extension.rs
@@ -1,10 +1,10 @@
 pub trait ResultExtension<T, E> {
-    fn flatten_std(self) -> Result<T, E>;
+    fn flatten_unstable(self) -> Result<T, E>;
 }
 
 impl<T, E> ResultExtension<T, E> for Result<Result<T, E>, E> {
     #[inline]
-    fn flatten_std(self) -> Result<T, E> {
+    fn flatten_unstable(self) -> Result<T, E> {
         self.and_then(std::convert::identity)
     }
 }
