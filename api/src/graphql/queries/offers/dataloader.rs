c7525a9d512577186813d82b818b6681c230facc
Anurag Singh <contact@anurag.sh>
2024-06-23 16:23:51 +0800

    fix unwrap

--- api/src/graphql/queries/offers/dataloader.rs
+++ api/src/graphql/queries/offers/dataloader.rs
@@ -1,6 +1,6 @@
 use async_graphql::dataloader::Loader;
 use entity::offer_details;
-use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+use sea_orm::{ColumnTrait, DatabaseConnection, DbErr, EntityTrait, QueryFilter};
 use std::{collections::HashMap, sync::Arc};
 
 pub struct OfferDetailsLoader {
@@ -9,15 +9,14 @@ pub struct OfferDetailsLoader {
 
 impl Loader<i64> for OfferDetailsLoader {
     type Value = offer_details::Model;
-    type Error = Arc<anyhow::Error>;
+    type Error = Arc<DbErr>;
 
     async fn load(&self, keys: &[i64]) -> Result<HashMap<i64, Self::Value>, Self::Error> {
         Ok(offer_details::Entity::find()
             .filter(offer_details::Column::PropositionId.is_in(keys.iter().copied()))
             .all(&self.database)
             .await
-            .map_err(Arc::new)
-            .unwrap()
+            .map_err(Arc::new)?
             .into_iter()
             .map(|o| (o.proposition_id, o))
             .collect::<HashMap<_, _>>())
