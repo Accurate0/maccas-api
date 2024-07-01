aa9128a2ccf7389ed6e534b5138feb4c432cf36e
Anurag Singh <contact@anurag.sh>
2024-07-01 20:35:39 +0800

    add upcoming offer query

--- api/src/graphql/queries/offers/mod.rs
+++ api/src/graphql/queries/offers/mod.rs
@@ -66,7 +66,52 @@ impl OffersQuery {
 
     async fn upcoming_offers<'a>(&self, ctx: &Context<'a>) -> async_graphql::Result<Vec<Offer>> {
         let db = ctx.data::<DatabaseConnection>()?;
-        Ok(vec![])
+        let now = chrono::offset::Utc::now().naive_utc();
+
+        let conditions = Condition::all().add(offers::Column::ValidFrom.gt(now));
+
+        let count_map = if ctx.look_ahead().field("count").exists() {
+            Some(
+                offers::Entity::find()
+                    .select_only()
+                    .join(JoinType::InnerJoin, offers::Relation::OfferDetails.def())
+                    .column(offer_details::Column::ShortName)
+                    .column_as(
+                        offer_details::Column::ShortName.count(),
+                        name_of!(count in OfferCount),
+                    )
+                    .filter(conditions.clone())
+                    .group_by(offer_details::Column::ShortName)
+                    .into_model::<OfferCount>()
+                    .all(db)
+                    .await?
+                    .into_iter()
+                    .map(|o| (o.short_name, o.count))
+                    .collect::<HashMap<_, _>>(),
+            )
+        } else {
+            None
+        };
+
+        Ok(offers::Entity::find()
+            .find_also_related(offer_details::Entity)
+            .filter(conditions)
+            .all(db)
+            .await?
+            .into_iter()
+            .map(|(offer, offer_details)| {
+                let count = if let Some(offer_details) = offer_details {
+                    count_map
+                        .as_ref()
+                        .and_then(|c| c.get(&offer_details.short_name).copied())
+                        .unwrap_or(0)
+                } else {
+                    0
+                };
+
+                Offer(offer, Some(count))
+            })
+            .collect())
     }
 
     async fn offers<'a>(&self, ctx: &Context<'a>) -> async_graphql::Result<Vec<Offer>> {
@@ -120,8 +165,7 @@ impl OffersQuery {
             .all(db)
             .await?
             .into_iter()
-            .map(|o| {
-                let (offer, offer_details) = o;
+            .map(|(offer, offer_details)| {
                 let count = if let Some(offer_details) = offer_details {
                     count_map
                         .as_ref()
