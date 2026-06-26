a33f1d77bb88c911adad192e240385a151a5ee95
Anurag Singh <contact@anurag.sh>
2026-06-26 19:37:09 +0800

    add logging

--- web/src/lib/server/featureflag.ts
+++ web/src/lib/server/featureflag.ts
@@ -4,8 +4,10 @@ import { env } from '$env/dynamic/private';
 
 const url = env.FEATURE_FLAGS_URL;
 if (url) {
+	console.log(`creating ff provider: ${url}`);
 	await OpenFeature.setProviderAndWait(new FeatureFlagProvider(url, 'maccas-web'));
 } else {
+	console.warn('falling back to noop provider');
 	await OpenFeature.setProviderAndWait(NOOP_PROVIDER);
 }
 
